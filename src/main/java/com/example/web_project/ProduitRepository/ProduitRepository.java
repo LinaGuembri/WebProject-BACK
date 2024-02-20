eff51d1e695d26479f0ab07f6b4410fe5c7d1f19
LinaGuembri <83191277+LinaGuembri@users.noreply.github.com>
2024-02-20 22:16:32 +0100

    Delete src/main/java/com/example/web_project/ProduitRepository directory

--- src/main/java/com/example/web_project/ProduitRepository/ProduitRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.web_project.ProduitRepository;
-
-import com.example.web_project.Entity.Produit;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProduitRepository extends JpaRepository<Produit, Long> {
-}
