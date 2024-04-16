54b4964fdbbb8bd2f4c0c81a956fba79a321dc7b
Mouna-Chtourou <mounachtourou2000@gmail.com>
2024-04-16 12:02:14 +0100

    gestion_User

--- src/main/java/com/example/web_project/Entity/User.java
+++ src/main/java/com/example/web_project/Entity/User.java
@@ -1,64 +1,30 @@
 package com.example.web_project.Entity;
+import com.example.web_project.Dto.UserDTO;
+import com.example.web_project.enums.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.NaturalId;
 
-
-import javax.persistence.*;
-
+@Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstname;
     private String lastname;
+    @NaturalId(mutable = true)
     private String email;
     private String telephone;
     private String password;
+    private UserRole userRole;
+    private boolean isEnabled = false;
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setIdr(Long id) {
-        this.id = id;
-    }
 
-    public String getFirstname() {
-        return firstname;
-    }
 
-    public void setFirstname(String fname) {
-        this.firstname = firstname;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
